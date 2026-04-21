c7a61dd840ebe7be83ebd5c386417897490161a7
Ankita Mishra <ankitamishra2617@gmail.com>
2026-04-21 14:36:12 +0530

    Insert data from MongoDB to node js

---
+++ insert.js
@@ -0,0 +1,16 @@
+const dbConnect = require('./mongodb');
+
+
+
+const insert = async () => {
+    let data = await dbConnect();
+    let result = await data.insertMany([
+        { name: "Nidhi", age: 30, email: "nidhi@gmail.com" },
+        { name: "Urvashi", age: 27, email: "urvashi@gmail.com" },
+        { name: "Garima", age: 26, email: "garima@gmail.com" }
+    ]);
+    if (result.acknowledged) {
+        console.log("Data Inserted");
+    }
+}
+insert()
