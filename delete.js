36823c9367c0c365e86f5d9dafda39400bd82a0a
Ankita Mishra <ankitamishra2617@gmail.com>
2026-04-21 14:52:04 +0530

    Delete data from MongoDB to node js

---
+++ delete.js
@@ -0,0 +1,15 @@
+const dbConnect = require('./mongodb');
+
+
+
+const deleteData = async () => {
+    let data = await dbConnect();
+    let result = await data.deleteMany(
+        { name: "Urvashi", age: 27, email: "urvashi@gmail.com" }
+
+    );
+    if (result.acknowledged) {
+        console.log("Data Deleted");
+    }
+}
+deleteData()
