071dbe846fa03a93d677ab2136b7914f7d0d408c
Ankita Mishra <ankitamishra2617@gmail.com>
2026-04-21 14:44:59 +0530

    Update data from MongoDb to node js

---
+++ update.js
@@ -0,0 +1,16 @@
+const dbConnect = require('./mongodb');
+
+
+
+const update = async () => {
+    let data = await dbConnect();
+    let result = await data.updateOne(
+        { name: "Nidhi"},
+        { $set: { age: 31}} 
+        
+    );
+    if (result.acknowledged) {
+        console.log("Data Updated");
+    }
+}
+update()
