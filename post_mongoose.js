629b46f7d22c868b36e7a0ecfb7eaa9f7d5a030c
Ankita Mishra <ankitamishra2617@gmail.com>
2026-04-22 12:26:49 +0530

    GET, DELETE AND PUT APIs with node js and mongoose

--- post_mongoose.js
+++ post_mongoose.js
@@ -12,4 +12,23 @@ app.post('/create', async (req, res) => {
     res.send("data received");
 })
 
+app.get('/list', async (req, res) => {
+    let data = await Product.find();
+    res.send(data);
+})
+
+app.delete('/delete/:_id', async (req, res) => {
+   console.log(req.params);   
+   let data = await Product.deleteOne({_id: req.params._id});
+    res.send(data);
+})
+
+app.put('/update/:_id', async (req, res) => {
+   console.log(req.params);   
+   let data = await Product.updateOne(
+    {_id: req.params._id},
+    {$set: req.body});
+    res.send(data);
+})
+
 app.listen(5000)
